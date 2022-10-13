fc7830c5afba1633bff69486d50313bda6deda58
esima <e.simatov@ya.ru>
2022-10-13 19:29:40 +0400

    Initial commit

---
+++ src/main/java/com/simatov/CinemaForumApp/CinemaForumAppApplication.java
@@ -0,0 +1,13 @@
+package com.simatov.CinemaForumApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaForumAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaForumAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simatov/CinemaForumApp/CinemaForumAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simatov.CinemaForumApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaForumAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
